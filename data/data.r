2f074e0eaff3437853ee2f51e9c33ff5f3081196
heath <heathyates@users.noreply.github.com>
2022-02-13 20:13:15 -0600

    Added materials for resume

---
+++ data/data.r
@@ -0,0 +1,41 @@
+#Skills
+skills <- tribble(
+  ~area, ~skills,
+  "Workbench", "Visual Studo Code, RStudio IDE, JupyterHub, Jupyter Notebooks",
+  "Python", "venv, pandas, numpy, scikit-learn",
+  "R", "renv, tidyverse, distill, ggplot2, rmarkdown, shiny", 
+  "Databases", "Teradata, Oracle, Mongo Studio 3T",
+  "GitOps", "Gitlab, Github, Helm, Rancher"
+)
+
+
+work <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Machine Learning Engineer", "Centene", "September", 2021, "Present", NA, "Remote", "Tools – Python, R, Shiny, SQL, Mongo, Helm, Rancher, and Gitlab", 
+  "Machine Learning Engineer", "Centene", "September", 2021, "Present", NA, "Remote", "Payment Integrity Application – Support an application that is focused on claims integrity that contributes to savings", 
+  "Machine Learning Engineer", "Centene", "September", 2021, "Present", NA, "Remote", "SSI Model – Support putting an SSI prospecting model that uses XGBoost into production that identifies members who are likely to qualify for SSI benefits", 
+  "Machine Learning Engineer", "Centene", "September", 2021, "Present", NA, "Remote", "Documentation – Support documentation for applications, models, and dashboards that team supports", 
+  "Machine Learning Engineer", "Centene", "September", 2021, "Present", NA, "Remote", "GitOps - Deploy applications into production onto kubernetes using gitlab, docker, helm configuration, and rancher", 
+  "Machine Learning Engineer", "Centene", "September", 2021, "Present", NA, "Remote", "Data Science Community – Member of data science continuing education leadership team at Centene which organizes outreach on R and python topics", 
+  "Adjunct Professor", "Centene", "September", 2018, "Present", NA, "Remote", "Research – Exploring new applications and approaches to applied data analytics", 
+  "Adjunct Professor", "Centene", "September", 2018, "Present", NA, "Remote", "Education – Developing materials to make applied data analytics more accessible", 
+  "Clinical Data Scientist", "Sunflower Health Plan", "August", 2018, "September", 2021, "Lenexa - Kansas", "Tools – R, Rmarkdown, SQL, Excel, and Gitlab ", 
+  "Clinical Data Scientist", "Sunflower Health Plan", "August", 2018, "September", 2021, "Lenexa - Kansas", "Clinical Reports and Dashboards – Monitored progress with assigned state paid for performance plans that led to substantial financial savings", 
+  "Clinical Data Scientist", "Sunflower Health Plan", "August", 2018, "September", 2021, "Lenexa - Kansas", "Statistical Reports – Implemented annual reports to track progress and provide data insights on state performance improvement plans", 
+  "Clinical Data Scientist", "Sunflower Health Plan", "August", 2018, "September", 2021, "Lenexa - Kansas", "Data Analytics – Ad hoc analysis for metrics related to quality and LTSS members", 
+  "Software Engineer", "Biosecurity Research Institute", "September", 2014, "July", 2018, "Manhattan - Kansas", "Tools – C#, .NET Core, SQL Server, and Excel",
+  "Software Engineer", "Biosecurity Research Institute", "September", 2014, "July", 2018, "Manhattan - Kansas", "Select Inventory Application – Application to track facility assets in a user interface and SQL server database",
+  "Software Engineer", "Biosecurity Research Institute", "September", 2014, "July", 2018, "Manhattan - Kansas", "Compliance Application – Application to track compliance with training and certification requirements for personnel at the facility"
+)
+
+
+#Education 
+edu <- tribble(
+  ~inst, ~degree, ~subject,
+  "Kansas State University", "Doctorate of Philosophy (PhD)", "Computer Science",
+  "Kansas State University", "Master of Science", "Statistics",
+  "Emporia State University", "Master of Science", "Mathematics",
+)
+
+
+#Publications 
